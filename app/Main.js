edd307cb62ec65209b4f01445edb5bdb61587572
joe kirkbride <joseph.kirkbride@gmail.com>
2023-05-23 14:11:28 -0400

    setup router

--- app/Main.js
+++ app/Main.js
@@ -1,17 +1,25 @@
 import React from "react"
 import ReactDOM from "react-dom/client"
+import { BrowserRouter, Routes, Route } from "react-router-dom"
+
 // custom React components
 import Header from "./components/Header"
 import HomeGuest from "./components/HomeGuest"
 import Footer from "./components/Footer"
+import About from "./components/About"
+import Terms from "./components/Terms"
 
 function MainComponent() {
   return (
-    <>
+    <BrowserRouter>
       <Header />
-      <HomeGuest />
+      <Routes>
+        <Route path='/' element={<HomeGuest />} />
+        <Route path='/about-us' element={<About />} />
+        <Route path='/terms' element={<Terms />} />
+      </Routes>
       <Footer />
-    </>
+    </BrowserRouter>
   )
 }
 
