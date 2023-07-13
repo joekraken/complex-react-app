1b25c98b43c28dfb4f8356f64a0f7d7f7b8a0368
joe kirkbride <joseph.kirkbride@gmail.com>
2023-07-13 15:31:29 -0400

    added tool tips to header when user logged in

--- app/components/HeaderLoggedIn.js
+++ app/components/HeaderLoggedIn.js
@@ -2,6 +2,7 @@ import React, { useContext, useEffect } from "react"
 import { Link } from "react-router-dom"
 import DispatchContext from "../DispatchContext"
 import StateContext from "../StateContext"
+import { Tooltip as ReactTooltip } from "react-tooltip"
 
 function HeaderLoggedIn(props) {
   const appDispatch = useContext(DispatchContext)
@@ -19,19 +20,22 @@ function HeaderLoggedIn(props) {
 
   return (
     <div className='flex-row my-3 my-md-0'>
-      <a onClick={handleSearchIcon} href='#' className='text-white mr-2 header-search-icon'>
+      <a onClick={handleSearchIcon} href='#' data-tooltip-content='Search' data-tooltip-id='search' className='text-white mr-2 header-search-icon'>
         <i className='fas fa-search'></i>
       </a>
-      <span className='mr-2 header-chat-icon text-white'>
+      <ReactTooltip place='bottom' id='search' className='custom-tooltip' />{" "}
+      <span data-tooltip-content='Chat' data-tooltip-id='chat' className='mr-2 header-chat-icon text-white'>
         <i className='fas fa-comment'></i>
         <span className='chat-count-badge text-white'> </span>
       </span>
-      <Link to={`/profile/${appState.user.username}`} className='mr-2'>
+      <ReactTooltip place='bottom' id='chat' className='custom-tooltip' />{" "}
+      <Link data-tooltip-content='My Profile' data-tooltip-id='profile' to={`/profile/${appState.user.username}`} className='mr-2'>
         <img className='small-header-avatar' src={appState.user.avatar} />
       </Link>
+      <ReactTooltip place='bottom' id='profile' className='custom-tooltip' />{" "}
       <Link className='btn btn-sm btn-success mr-2' to='/create-post'>
         Create Post
-      </Link>
+      </Link>{" "}
       <button onClick={handleLogout} className='btn btn-sm btn-secondary'>
         Sign Out
       </button>
