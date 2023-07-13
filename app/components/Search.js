8ee9df1a39f1ad676376f0cb99faf3c333d7b2cd
joe kirkbride <joseph.kirkbride@gmail.com>
2023-07-13 16:26:01 -0400

    listen for input changes to search term

--- app/components/Search.js
+++ app/components/Search.js
@@ -1,15 +1,67 @@
 import React, { useContext, useEffect } from "react"
 import DispatchContext from "../DispatchContext"
 import StateContext from "../StateContext"
+import { useImmer } from "use-immer"
+import Axios from "axios"
 
 function Search(props) {
   const appDispatch = useContext(DispatchContext)
   const appState = useContext(StateContext)
 
-  useEffect(() => {
-    // listen for key press
-    document.addEventListener("keyup", searchKeyPressHandler)
+  // like set state
+  const [state, setState] = useImmer({
+    searchTerm: "",
+    results: [],
+    show: "neither",
+    requestCount: 0
+  })
 
+  // listen for search input change
+  useEffect(() => {
+    if (state.searchTerm.trim()) {
+      setState(draft => {
+        draft.show = "loading"
+      })
+      const delay = setTimeout(() => {
+        setState(draft => {
+          draft.requestCount++
+        })
+      }, 800)
+
+      // clean up function clears timeout if key pressed before timer ends
+      return () => clearTimeout(delay)
+    } else {
+      setState(draft => {
+        draft.show = "neither"
+      })
+    }
+  }, [state.searchTerm])
+
+  // listen for requests, then call backend
+  useEffect(() => {
+    if (state.requestCount) {
+      const requestController = new AbortController()
+      async function fetchResults() {
+        try {
+          const response = await Axios.post("/search", { searchTerm: state.searchTerm }, { signal: requestController.signal })
+          setState(draft => {
+            draft.results = response.data
+            draft.show = "results"
+          })
+        } catch (e) {
+          console.log("Error occured fetching search results.")
+          console.log(e)
+        }
+      }
+      fetchResults()
+      // clean up
+      return () => requestController.abort()
+    }
+  }, [state.requestCount])
+
+  // listen for key press
+  useEffect(() => {
+    document.addEventListener("keyup", searchKeyPressHandler)
     // return a clean up function to remove event listener
     return () => document.removeEventListener("keyup", searchKeyPressHandler)
   }, [])
@@ -22,6 +74,13 @@ function Search(props) {
     }
   }
 
+  function handleInput(e) {
+    const value = e.target.value
+    setState(draft => {
+      draft.searchTerm = value
+    })
+  }
+
   return (
     <div className='search-overlay'>
       <div className='search-overlay-top shadow-sm'>
@@ -29,7 +88,7 @@ function Search(props) {
           <label htmlFor='live-search-field' className='search-overlay-icon'>
             <i className='fas fa-search'></i>
           </label>
-          <input autoFocus type='text' autoComplete='off' id='live-search-field' className='live-search-field' placeholder='What are you interested in?' />
+          <input onChange={handleInput} autoFocus type='text' autoComplete='off' id='live-search-field' className='live-search-field' placeholder='What are you interested in?' />
           <span onClick={() => appDispatch({ type: "closeSearch" })} className='close-live-search'>
             <i className='fas fa-times-circle'></i>
           </span>
@@ -38,23 +97,13 @@ function Search(props) {
 
       <div className='search-overlay-bottom'>
         <div className='container container--narrow py-3'>
-          <div className='live-search-results live-search-results--visible'>
+          <div className={"circle-loader " + (state.show == "loading" ? "circle-loader--visible" : "")}></div>
+          <div className={"live-search-results " + (state.show == "results" ? "live-search-results--visible" : "")}>
             <div className='list-group shadow-sm'>
               <div className='list-group-item active'>
-                <strong>Search Results</strong> (3 items found)
+                <strong>Search Results</strong> ({state.results.length} {state.results.length == 1 ? "item" : "items"} found)
               </div>
-              <a href='#' className='list-group-item list-group-item-action'>
-                <img className='avatar-tiny' src='https://gravatar.com/avatar/b9408a09298632b5151200f3449434ef?s=128' /> <strong>Example Post #1</strong>
-                <span className='text-muted small'>by brad on 2/10/2020 </span>
-              </a>
-              <a href='#' className='list-group-item list-group-item-action'>
-                <img className='avatar-tiny' src='https://gravatar.com/avatar/b9216295c1e3931655bae6574ac0e4c2?s=128' /> <strong>Example Post #2</strong>
-                <span className='text-muted small'>by barksalot on 2/10/2020 </span>
-              </a>
-              <a href='#' className='list-group-item list-group-item-action'>
-                <img className='avatar-tiny' src='https://gravatar.com/avatar/b9408a09298632b5151200f3449434ef?s=128' /> <strong>Example Post #3</strong>
-                <span className='text-muted small'>by brad on 2/10/2020 </span>
-              </a>
+              {state.results.map(post => {})}
             </div>
           </div>
         </div>
